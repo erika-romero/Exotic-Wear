6f9b4fb23e39eaaf63e0c1762be48777fbfa6728
Erika Romero <84427277+erika-romero@users.noreply.github.com>
2022-05-17 17:28:36 -0500

    bd

--- components/catalogo/verificador.php
+++ components/catalogo/verificador.php
@@ -1,3 +1,5 @@
 <?php 
 print_r($_GET);
+$ClientID="AT0xqtQTZEvYJ6_aokWIVvW2GsP5EAogcndAvXgYhKfWa6N7XuXDNEQvTE1zRvzvEGRposSvzxtNRxZh";
+$Secret="EAf2gegJvgJjBaPAGjExa8ozLCMXgEL_RhKTbpyyb-yuNNyVKZeO7QDI8VGgV2V34H_i7XG0o-Bw36I2";
 ?>
